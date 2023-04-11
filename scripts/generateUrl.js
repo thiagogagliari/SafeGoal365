0b4234b4b92004908a63c7770e6e4645ecda0c77
thiagogagliari <thiagogagliari@hotmail.com>
2023-04-11 11:24:28 -0300

    Pimeiro commit

---
+++ scripts/generateUrl.js
@@ -0,0 +1,15 @@
+// function generateRandomString(length) {
+//     const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     let result = '';
+//     for (let i = 0; i < length; i++) {
+//         result += chars.charAt(Math.floor(Math.random() * chars.length));
+//     }
+//     return result;
+// }
+
+// const randomString = generateRandomString(10); // Gera uma string aleatória com 10 caracteres
+// const url = `http://example.com/${randomString}`;
+
+// console.log(url);
+
+// 2EYoKS4u4Z.html
---
+++ scripts/login.js
@@ -0,0 +1,46 @@
+const users = [
+    {
+        id: '1',
+        login: 'thiagogagliari',
+        pass: 'Thisam@y100512'
+    },
+    {
+        id: '2',
+        login: 'igorvidal',
+        pass: 'igorvl'
+    },
+    {
+        id: '3',
+        login: 'samuelalmd93',
+        pass: 'Dino6740'
+    },
+    {
+        id: '4',
+        login: 'CLZpaulinho',
+        pass: '413721'
+    }
+]
+
+function logar() {
+    let getUsers = document.getElementById('login').value;
+    let getPassword = document.getElementById('senha').value;
+    let validLogin = false;
+
+
+    for (let i in users) {
+        if (getUsers == users[i].login && getPassword == users[i].pass) {
+            validLogin = true
+            break;
+        } else {
+            validLogin = false
+        }
+    }
+
+    if (validLogin == true) {
+        alert(`Seja bem-vindo ${getUsers}`);
+        location.href = 'pages/2EYoKS4u4Z.html';
+    } else {
+        alert('Usuário e/ou senha inválidos!');
+    }
+
+};

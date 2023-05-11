fd1c41b5941d2d581a122ba8258ef1aedb7616a9
thiagogagliari <thiagogagliari@hotmail.com>
2023-05-11 18:00:17 -0300

    update

--- scripts/login.js
+++ scripts/login.js
@@ -2,57 +2,68 @@ const users = [
     {
         id: '1',
         login: 'thiagogagliari',
-        pass: '19852810'
+        pass: '19852810',
+        name: 'Thiago Gagliari'
     },
     {
         id: '2',
         login: 'igorvidal',
-        pass: 'igorvl'
+        pass: 'igorvl',
+        name: 'Igor Vidal'
     },
     {
         id: '3',
         login: 'samuelalmd93',
-        pass: 'Dino6740'
+        pass: 'Dino6740',
+        name: 'Samuel Almeida'
     },
     {
         id: '4',
         login: 'CLZpaulinho',
-        pass: '413721'
+        pass: '413721',
+        name: 'Paulo Edson'
     },
     {
         id: '5',
         login: 'dagodf',
-        pass: 'Dago2023'
+        pass: 'Dago2023',
+        name: 'Dagoberto Corrêa'
     },
     {
         id: '6',
         login: 'nestorgarcia',
-        pass: 'ongarcia'
+        pass: 'ongarcia',
+        name: 'Nestor García'
     },
     {
         id: '7',
         login: 'maurirodrigues',
-        pass: 'malice1'
+        pass: 'malice1',
+        name: 'Mauri Rodrigues'
     },
     {
         id: '8',
         login: 'rouger',
-        pass: '1978'
+        pass: '1978',
+        name: 'Rouger Iurk'
     },
     {
         id: '9',
         login: 'eng.michel2013',
-        pass: '15081963'
+        pass: '15081963',
+        name: 'Michel'
     },
     {
         id: '10',
         login: 'wandrey',
-        pass: 'Oliver154263'
+        pass: 'Oliver154263',
+        name: 'Wandrey'
     },
     {
         id: '11',
         login: 'amontos2017',
-        pass: '55651331oi'
+        pass: '55651331oi',
+        name: 'Marcelo Alves'
     }
 ];
 
